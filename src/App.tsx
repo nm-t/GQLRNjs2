0b15725774ef5e14a6524c1cf126c0e1db8ebff2
Nathalia Tan <nathalia.tan@deloitte.com>
2019-04-28 11:03:17 +1000

    Step 1: Import Apollo tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
-import { ApolloProvider } from "react-apollo";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { client } from "./utils/createApolloClient";
 import MovieList from "./components/MovieList/MovieList";
 import Movie from "./components/Movie/Movie";
 import { setUserId } from "./utils/userIdHelper";
@@ -13,14 +11,12 @@ const App = () => {
   });
 
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div className="App">
-          <Route path="/" exact component={MovieList} />
-          <Route path="/movie/:id" component={Movie} />
-        </div>
-      </Router>
-    </ApolloProvider>
+    <Router>
+      <div className="App">
+        <Route path="/" exact component={MovieList} />
+        <Route path="/movie/:id" component={Movie} />
+      </div>
+    </Router>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,25 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import ApolloClient from "apollo-boost"; // import default; you can name this whatever and it'll still work
+import { ApolloProvider } from "react-apollo"; // import non-default class
+
 // create a new ApolloClient, which is from "apollo-boost";
 // pass {uri: "GraphQL endpoint"} as the input for ApolloClient
+const client = new ApolloClient({
+    uri: "https://serene-basin-84996.herokuapp.com/"
+});
 
 // use ApolloProvider as the Provider, which is from "react-apollo"
 // Passing "client" to ApolloProvider
 // wrapper App with ApolloProvider
+const ApolloApp = () => (
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+)
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<ApolloApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
